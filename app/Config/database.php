5c9a99f3f4d5665b0528935150b621212bfc7399
kefear <yy@Mac-mini-Evgenij-3.local>
2014-10-15 11:30:16 +0600

    Изменения в доступе к базе
    добали базу для живого сайта и проверку что бы автоматизировать
    переключение

--- app/Config/database.php
+++ app/Config/database.php
@@ -9,17 +9,41 @@ class DATABASE_CONFIG {
 		'password' => '',
 		'database' => 'cake_control_project',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 
-	public $test = array(
+	public $dev_yy = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'cake_control_project',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
+
+	public $live = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'personag_cp',
+		'password' => '(@tmtrEF3%I)',
+		'database' => 'personag_cp',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+
+	public function __construct() {
+		if (strpos(env('HTTP_HOST'), 'dev.idq.kz') !== false) {
+			// use site_one database config
+			$this->default = $this->dev_yy;
+
+		// elseif site_two
+		} else {
+
+			// use site_two database config
+			$this->default = $this->live;
+		}
+	}
 }
